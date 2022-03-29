47515fe9850128488a7d530ef4ae64560afdec6a
Aj Ishac <96703509+Aj-Ishac@users.noreply.github.com>
2022-03-29 17:14:05 -0500

    Add files via upload

--- main_device/Master_Device.ino
+++ main_device/Master_Device.ino
@@ -28,6 +28,10 @@ void setup()
   
   delay(1000);
 
+  /*
+   * On Startup - Play Buzzer Tune indicating successful startup to the user.
+   */
+
   pinMode(Trigger_Pin, OUTPUT);
   pinMode(Echo_PIN, INPUT);
   pinMode(Buzzer_Pin, OUTPUT);
@@ -45,32 +49,78 @@ void loop()
   byte DHT11_Humidity;
   int Battery;
 
-  String BTData;
+  /* user inputs: metric convert button
+   * BTreceives = "mC"
+   * bool isMetric = true, triggers metricConvert function on loop from now on
+   * 
+   * user inputs: imperial convert button
+   * BTreceives = "iC"
+   * bool isMetric = off, triggers imperialConvert function on loop from now on
+   */
+   
+  String BTreceive;
+  bool isMetric = false;
+  if(BT.available())
+  {
+    BTreceive = BT.read();
+    if(BTreceive == "mC")
+    {
+      isMetric = true;
+      Serial.print("isMetric: ");
+      Serial.println(isMetric);
+    }
+    else if(BTreceive == "iC")
+    {
+      isMetric = false;
+      Serial.print("isMetric: ");
+      Serial.println(isMetric);
+    } 
+  }
 
-  Ultrasonic_CollisionDetect(Trigger_Pin, Echo_PIN, Buzzer_Pin, Distance, BuzzerFreq, isBeeping);
+  Ultrasonic_CollisionDetect(Trigger_Pin, Echo_PIN, Buzzer_Pin, Distance, BuzzerFreq, isBeeping, isMetric);
   TempHumidity_Detect(DHT_Pin,DHT11_Temperature,DHT11_Humidity);
 
   //add Buzzer_Frequency to led blink frequency parameter
   LED_Blink(RedLED_Pin);                    
 
-  //temp wire data transfer from master to slave device
-  Wire.beginTransmission (SLAVE_ADDRESS);
-  I2C_writeAnything (Distance);
-  I2C_writeAnything (BuzzerFreq);
-  I2C_writeAnything (isBeeping);
-  I2C_writeAnything (DHT11_Temperature);
-  I2C_writeAnything (DHT11_Humidity);
-  Wire.endTransmission ();
-
-  //distance;temp;humidity;battery string bluetooth send
-  BTData = String(Distance) + ";" + String(DHT11_Temperature) + ";" + String(DHT11_Humidity) + ";" + String(Battery); 
-  Serial.print(BTData);
-  BT.println(BTData);
+  //string BTsend = distance;temp;humidity;battery
+  String BTsend;
+  BTsend = (Distance);
+  BTsend += ";";
+  BTsend += DHT11_Temperature;
+  BTsend += ";";
+  BTsend += DHT11_Humidity;
+  BTsend += ";";
+  BTsend += Battery;
   
-  delay(400);
+  Serial.print(BTsend);
+  BT.println(BTsend);
 }
 
-void Ultrasonic_CollisionDetect(int, int, int,float &Distance,int &BuzzerFreq, bool &isBeeping) 
+float distance_Convert(long duration, int modifier)
+{
+  /* modifier = 0
+   * raw distance -> imperial
+   * 
+   * modifier = 1
+   * raw distance -> metric
+   */
+  
+  int distance;
+  if(modifier == 0) //imperial convert
+  {
+    distance = (duration/2) / 74;
+  }
+  else if(modifier == 1) //metric convert
+  {
+    distance = (duration * 0.034) / 2;
+  }
+  return distance;
+}
+
+void tempHumidity_Convert(
+
+void Ultrasonic_CollisionDetect(int, int, int,float &Distance,int &BuzzerFreq, bool &isBeeping, bool &isMetric) 
 {
   digitalWrite(Trigger_Pin, LOW);
   delayMicroseconds(2);
@@ -78,15 +128,24 @@ void Ultrasonic_CollisionDetect(int, int, int,float &Distance,int &BuzzerFreq, b
   delayMicroseconds(10);
   digitalWrite(Trigger_Pin, LOW);
 
-  //IF CHANGED, MAKE SURE TO REFLECT CHANGES IN void BuzzerBeep IN SLAVE_DEVICE.CPP
-  int Min_Detection = 20;                 //***Min Detection Controller***
+  int Min_Detection = 20;                 //***Min Detection Controller***--
   int Max_Detection = 55;                 //***Max Detection Controller***
 
   long Duration;                          
-  
   Duration = pulseIn(Echo_PIN, HIGH);     //Duration signal takes to traverse from TriggerPin to EchoPin
-  Distance = (Duration * 0.034) / 2;      //Distance calculation in cm - currently linear. needs to be exponential for satisfying feedback 
-  BuzzerFreq = Distance * 5;              //***Buzzer Frequency Controller** current base value: Distance * 10;
+
+  //calculate distance in inches or cms depending on isMetric bool status
+  if(isMetric = true)
+  {
+    Distance = cvt_to_metric(Duration);
+  }
+  else
+  {
+    Distance = cvt_to_imperial(Duration);
+  }
+  
+  //Distance = (Duration * 0.034) / 2;      //Distance calculation in cm - currently linear. needs to be exponential for satisfying feedback 
+  BuzzerFreq = Distance * 5;                //***Buzzer Frequency Controller** current base value: Distance * 10;
   
   //no buzzer beeps when !distance above max detection!
   if (Distance > Max_Detection){          
@@ -189,13 +248,20 @@ void LED_Blink(int LED_Input)
 
 //main:
 //main device will send and receive BT data
+
 //buzzer beep needs to be %based of distance so beep frequency is unaffected by changing min/max detection values
 //receive BT data of min/max detection value changes and apply here, and onto slave device on the next slave startup
 //battery% to implement low battery warning in LED blinking feedback, and for BT data sent to phone for data relay
 //button/switch to turn power off
+//on-startup buzzer tune
+//update distance consistently, update rest every 5secs
+//BT: on receive BT string imperial/metric, convert distance/temp from program before sending
+//IDLE mode
+//battery saver mode
 
 //slave:
 //slave device will only receive BT data
+
 //on startup: check if min/max detection values have been changed since last use, adjust if so
 //buzzer beep needs to be %based of distance
 //button press to enable idle/low battery mode -> turn off lcd backlight till next normal press
@@ -203,13 +269,13 @@ void LED_Blink(int LED_Input)
 
 //android:
 //android app will send and receive BT data
+
 //screen 0, establish BT connection -> brief intro goes here
 //screen 1, data relay -> BT RSSI chart
 //                     -> Receive Battery% and map it to block value
 //                     -> Overlay: Warning when distance is within alert range
 //                     -> Warning Method: Ring and/or Vibrate, or Mute
 //screen 2, Options    -> Static Page: Explain data points relayed
-//screen 3, Diags      -> Yet to implement, log console goes here
 //screen 4, Configs    -> Alerts: Range,
 //                     -> Unit Convert Button, change Distance+m to Distance+ft/inches to notify user
 //                     -> if in Imperial units, change to metric in the backend before BT send to main device
@@ -221,18 +287,4 @@ void LED_Blink(int LED_Input)
 //wrap up:
 //documentation
 //housing of main and slave device
-//magnet
-
-
-//BT
-//THIS IS WHAT WORKS FOR MASTER
-//http://phillipecantin.blogspot.com/2014/08/hc-05-bluetooth-link-with-zero-code.html
-//https://www.youtube.com/watch?v=hyME1osgr7s
-//https://howtomechatronics.com/tutorials/arduino/how-to-configure-pair-two-hc-05-bluetooth-module-master-slave-commands/
-//http://www.martyncurrey.com/connecting-2-arduinos-by-bluetooth-using-a-hc-05-and-a-hc-06-pair-bind-and-link/
-
-//https://www.instructables.com/How-to-Configure-HC-05-Bluetooth-Module-As-Master-/
-//POTENTIAL FIX TO BT SHITSHOW
-
-//TRANSFER DATA THROUGH ARDUINOS THROUGH WIRES
-//www.youtube.com/watch?v=PnG4fO5_vU4         
+//magnet         
